f057337efeec55a6c42850839c863f4b4517f78e
Clinton Alexander <admin@clintonio.com>
2015-04-05 17:42:01 +0100

    Renamed mod main class from example name.

--- src/main/java/com/natureoverhaul/NatureOverhaul.java
+++ src/main/java/com/natureoverhaul/NatureOverhaul.java
@@ -7,20 +7,19 @@ import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Instance;
-import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.common.MinecraftForge;
 
-@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = NatureOverhaul.MODID, version = NatureOverhaul.VERSION)
+public class NatureOverhaul
 {
     public static final String MODID = "natureoverhaul";
     public static final String VERSION = "1.1";
 
-    @Instance(value = ExampleMod.MODID)
-    public static ExampleMod instance;
+    @Instance(value = NatureOverhaul.MODID)
+    public static NatureOverhaul instance;
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event) {}
@@ -33,12 +32,8 @@ public class ExampleMod
     }
 
     @EventHandler
-    public void load(FMLInitializationEvent event) {
-
-    }
+    public void load(FMLInitializationEvent event) {}
 
     @EventHandler
-    public void postInit(FMLPostInitializationEvent event) {
-
-    }
+    public void postInit(FMLPostInitializationEvent event) {}
 }
